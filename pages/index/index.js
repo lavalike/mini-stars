06f1039fa944e32229131dad97d814b5d787880d
wangzhen <lavalike@yeah.net>
2022-01-29 23:26:12 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,19 @@
+// index.js
+// 获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    pictures: [
+      "../../res/images/one.jpg",
+      "../../res/images/two.jpg",
+      "../../res/images/three.jpg",
+      "../../res/images/four.jpg",
+      "../../res/images/five.jpg"
+    ]
+  },
+
+  onLoad() {
+
+  },
+})
